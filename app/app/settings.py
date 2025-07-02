d513ba71937e0e4583536a3ec4e3174d2e0a5050
daninesvera <daninesvera@gmail.com>
2025-07-01 23:53:03 -0300

    increase refresh token time

--- app/app/settings.py
+++ app/app/settings.py
@@ -151,7 +151,7 @@ CORS_ALLOWED_ORIGINS = [
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
-    "SLIDING_TOKEN_REFRESH_LIFETIME": timedelta(days=1),
+    "SLIDING_TOKEN_REFRESH_LIFETIME": timedelta(days=30),
     "SLIDING_TOKEN_LIFETIME": timedelta(days=30),
     "SLIDING_TOKEN_REFRESH_LIFETIME_LATE_USER": timedelta(days=1),
     "SLIDING_TOKEN_LIFETIME_LATE_USER": timedelta(days=30),
